23a3251c27ce87cb3386bc4aeb2b418a5bce3bfe
Rasulwebs <boboqulovrasul80@gamil.com>
2022-08-26 19:44:53 +0500

    js_masalaa

---
+++ jsss/homejsss.js
@@ -0,0 +1,56 @@
+"use strict";
+
+
+//=============== 1 masala================
+
+// var numbers=[1, 3, 5, 9];
+// let sum=0;
+
+// for(let i=0; i<numbers.length; i++){
+//     sum+=numbers[i]
+
+// }
+
+// console.log(sum);
+
+
+
+// ============== 2 masla ==============
+
+
+
+// let money =+prompt('Soqqa')
+
+// if (money > 50 && money < 300) {
+
+//     console.log(money+money*0.15)
+
+// } else if (money < 50 ) {
+
+//     console.log(money + (money * 0.2))
+
+// }else if (money > 300 ) {
+
+//     console.log(money + (money * 0.2))
+// }
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
